dfbe2795092c75d61f6d2e04363ac03e4425b319
alfisyahrin <alfisyahrinc@gmail.com>
2019-07-02 14:55:17 +0700

    Implement launchscreen with logo pokemon

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
+import { StatusBar, Image, View } from 'react-native'
 import { Images } from '../Themes'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.navigation.navigate("PokedexListScreen");
+    }, 2000);
+  }
+
   render () {
-    return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
+    return (      
+      <View style={styles.container}>
+        <StatusBar backgroundColor={"#f1c40f"} barStyle='light-content' />
+          <View style={styles.section}>
+            <Image
+                style={styles.Image}
+                source={require('../Images/logoPokemon.png')}
+            />
           </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-
-        </ScrollView>
-      </View>
+        </View>
     )
   }
 }
+
+ 
--- App/Containers/Styles/LaunchScreenStyles.js
+++ App/Containers/Styles/LaunchScreenStyles.js
@@ -3,16 +3,27 @@ import { Metrics, ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
-  container: {
-    paddingBottom: Metrics.baseMargin
-  },
   logo: {
     marginTop: Metrics.doubleSection,
     height: Metrics.images.logo,
     width: Metrics.images.logo,
-    resizeMode: 'contain'
+    resizeMode: "contain"
   },
   centered: {
-    alignItems: 'center'
-  }
+    alignItems: "center"
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  section:{
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',
+  },
+  Image: {
+    height: Metrics.images.logo,
+    width: Metrics.images.logo,
+    resizeMode: 'contain'
+  },
 })
