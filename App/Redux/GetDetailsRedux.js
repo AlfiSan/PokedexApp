04e59eae97ee3bc3294ec52c110b9b886ea52aad
alfisyahrin <alfisyahrinc@gmail.com>
2019-07-02 15:23:18 +0700

    Generate file redux & sagas Getlist and GetDetails

---
+++ App/Redux/GetDetailsRedux.js
@@ -0,0 +1,52 @@
+import { createReducer, createActions } from 'reduxsauce'
+import Immutable from 'seamless-immutable'
+
+/* ------------- Types and Action Creators ------------- */
+
+const { Types, Creators } = createActions({
+  getDetailsRequest: ['data'],
+  getDetailsSuccess: ['payload'],
+  getDetailsFailure: null
+})
+
+export const GetDetailsTypes = Types
+export default Creators
+
+/* ------------- Initial State ------------- */
+
+export const INITIAL_STATE = Immutable({
+  data: null,
+  fetching: null,
+  payload: null,
+  error: null
+})
+
+/* ------------- Selectors ------------- */
+
+export const GetDetailsSelectors = {
+  getData: state => state.data
+}
+
+/* ------------- Reducers ------------- */
+
+// request the data from an api
+export const request = (state, { data }) =>
+  state.merge({ fetching: true, data, payload: null })
+
+// successful api lookup
+export const success = (state, action) => {
+  const { payload } = action
+  return state.merge({ fetching: false, error: null, payload })
+}
+
+// Something went wrong somewhere.
+export const failure = state =>
+  state.merge({ fetching: false, error: true, payload: null })
+
+/* ------------- Hookup Reducers To Types ------------- */
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.GET_DETAILS_REQUEST]: request,
+  [Types.GET_DETAILS_SUCCESS]: success,
+  [Types.GET_DETAILS_FAILURE]: failure
+})
