eed6128bbef2c7ec7abe5fd018a18fae5ecf21b2
alfisyahrin <alfisyahrinc@gmail.com>
2019-07-02 15:53:06 +0700

    Prepare for redux and saga to get list

--- App/Containers/PokedexListScreen.js
+++ App/Containers/PokedexListScreen.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
-import { connect } from 'react-redux'
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  ActivityIndicator
+} from 'react-native';
+import PokemonList from '../Components/PokedexBasicInfo';
+import { connect } from 'react-redux';
+import { NavigationActions } from 'react-navigation';
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 
@@ -10,22 +20,38 @@ import styles from './Styles/PokedexListScreenStyle'
 class PokedexListScreen extends Component {
   render () {
     return (
-      <ScrollView style={styles.container}>
-        <KeyboardAvoidingView behavior='position'>
-          <Text>PokedexListScreen</Text>
-        </KeyboardAvoidingView>
+      <View style={styles.container}>
+      <StatusBar backgroundColor={"#f1c40f"} translucent barStyle="light" />
+      <View style={styles.viewTitle}>
+        <Text style={styles.titleText}>
+          Pokemon}
+        </Text>
+      </View>
+      { (loading) &&
+          <View style={styles.loading}>
+            <ActivityIndicator size='large' color={"#f1c40f"} />
+          </View>
+      }
+      <ScrollView contentContainerStyle={styles.viewList}>
+        <PokemonList
+            
+          />
       </ScrollView>
+      </View>
     )
   }
 }
 
 const mapStateToProps = (state) => {
   return {
+    loading: state.listPokedex.loading,
+    data: state.listPokedex.data,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    dispatch
   }
 }
 
--- App/Containers/Styles/PokedexListScreenStyle.js
+++ App/Containers/Styles/PokedexListScreenStyle.js
@@ -1,6 +1,39 @@
-import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { StyleSheet, Dimensions} from 'react-native';
+import { Colors, Fonts } from '../../../Themes/index';
+const { width, height } = Dimensions.get('window')
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
-})
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  viewTitle:{
+    margin: 16,
+    justifyContent: 'center',
+    padding: 10,
+    alignItems: 'center'
+  },
+  titleText:{
+    textAlign: 'left',
+    color: Colors.black,
+    paddingRight: 8,
+    lineHeight: 24
+  },
+  loading: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 10
+  },
+  viewList:{
+    paddingBottom: 16,
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+});
